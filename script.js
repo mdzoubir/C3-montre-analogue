6aeee8f0b18fabe22a4d81b2ebce31a52908ccbf
mohamed zoubir <simozoubir012@gmail.com>
2020-04-11 21:43:42 +0100

    ajouter un nouveux modi

--- script.js
+++ script.js
@@ -3,33 +3,19 @@ let AIGUILLEHR = document.querySelector("#hour");
 let AIGUILLEMIN = document.querySelector("#minute");
 let AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
-let date = new Date();
-let hour = date.getHours();
-let min = date.getMinutes();
-let second = date.getSeconds();
-//Ajouter l'heure , minite , seconde  dans des varaiables
-let hourDeg = 1 / 120;
-let minDeg = 1 / 10;
-let secondDeg = 6;
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-let positionHr = ((hour * 3600) + (min * 60) + second) * hourDeg;
-let positionMin = ((min * 60) + second) * minDeg;
-let positionSecond = second * secondDeg;
-
-// Déplacer les aiguilles 
 function demarrerLaMontre() {
-    positionSecond += secondDeg;
-    positionMin += minDeg;
-    positionHr += hourDeg;
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let second = date.getSeconds();
 
-    AIGUILLEHR.style.transform = "rotate(" + positionHr + "deg)";
-    AIGUILLEMIN.style.transform = "rotate(" + positionMin + "deg)";
-    AIGUILLESEC.style.transform = "rotate(" + positionSecond + "deg)";
+    let secondDeg = 6 * second;
+    let minDeg = 6 * min + (1 / 60) * secondDeg;
+    let hourDeg = 30 * hour + (1 / 60) * minDeg;
 
-
+    AIGUILLEHR.style.transform = `rotate(${hourDeg}deg)`;
+    AIGUILLEMIN.style.transform = `rotate(${minDeg}deg)`;
+    AIGUILLESEC.style.transform = `rotate(${secondDeg}deg)`;
 }
 // Exercuter la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
